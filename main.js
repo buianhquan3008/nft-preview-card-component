ce86e404094288166704178beb2691c3c61542ae
vera <vera@10kn.com>
2022-09-15 01:33:57 +0700

    hover image

---
+++ main.js
@@ -0,0 +1,8 @@
+// var imgBg = document.getElementById('img-background');
+// var view = document.getElementById('view');
+
+// imgBg.addEventListener('mouseenter', function () {
+//     view.style.display = 'block';
+// });
+
+// console.log(view);
